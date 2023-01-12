28d9bd644a9e31fee1b38df4af9f52d7e3c10d67
Seblis <seblis91@gmail.com>
2023-01-12 18:28:46 +0100

    Login form logic separated in order to change style

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,10 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.forms import TextInput, PasswordInput, CharField
+from django.contrib.auth.forms import (
+    UserChangeForm,
+    UserCreationForm,
+    AuthenticationForm,
+    UsernameField
+)
 
 from .models import LJUser
 
@@ -13,3 +19,28 @@ class LJUserChangeForm(UserChangeForm):
     class Meta:
         model = LJUser
         fields = UserChangeForm.Meta.fields
+
+
+class LJUserLoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super(LJUserLoginForm, self).__init__(*args, **kwargs)
+        self.label_suffix = ''
+
+    username = UsernameField(
+        widget=TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Username',
+                'id': 'floatingInput',
+            }
+        )
+    )
+    password = CharField(
+        widget=PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Password',
+                'id': 'floatingPassword',
+            }
+        )
+    )
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView
+
 from . import views
+from .forms import LJUserLoginForm
 
 app_name = 'accounts'
 urlpatterns = [
-    path('signup/', views.SignUpView.as_view(), name='signup')
+    path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(
+        template_name='registration/login.html',
+        authentication_form=LJUserLoginForm
+    ), name='login'),
 ]
